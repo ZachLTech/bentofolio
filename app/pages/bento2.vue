3159aabb04ef82b3c943f1c6efefb7e94457a80d
ZachLTech <zlopezzl2021@gmail.com>
2024-08-02 00:56:59 -0400

    setup w/ icons & gradients

--- app/pages/bento2.vue
+++ app/pages/bento2.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="w-screen h-screen p-[100px] flex justify-center items-center">
       <div class="w-full h-full grid grid-cols-7 grid-rows-8 gap-5">
-          <div class="w-full h-full col-span-2 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
-          <div class="w-full h-full col-span-3 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
-          <div class="w-full h-full col-span-2 row-span-8 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
-          <div class="w-full h-full col-span-5 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+          <div class="w-full h-full col-span-2 row-span-4 flex justify-center items-center font-bold text-5xl bg-gradient-to-br from-[#171717] via-[#101010] to-black rounded-3xl shadow-lg"></div>
+          <div class="w-full h-full col-span-3 row-span-4 flex justify-center items-center font-bold text-5xl bg-gradient-to-br from-[#171717] via-[#101010] to-black rounded-3xl  shadow-lg"></div>
+          <div class="w-full h-full col-span-2 row-span-8 flex justify-center items-center font-bold text-5xl bg-gradient-to-br from-[#171717] via-[#101010] to-black rounded-3xl  shadow-lg"></div>
+          <div class="w-full h-full col-span-5 row-span-4 flex justify-center items-center font-bold text-5xl bg-gradient-to-br from-[#171717] via-[#101010] to-black rounded-3xl  shadow-lg"></div>
       </div>
+      <p class="absolute mb-[-70px] self-end text-3xl text-[#383838]">ssh zachl.tech</p>
   </div>
 </template>
