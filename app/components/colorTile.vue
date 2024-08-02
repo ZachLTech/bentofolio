7803b35d0c7832b5b28ca888e87870bf1ee2efd0
ZachLTech <zlopezzl2021@gmail.com>
2024-08-02 00:06:00 -0400

    init

---
+++ app/components/colorTile.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :class="'w-[20vw] h-[80vh] flex justify-center items-center font-bold text-5xl bg-[' + background + 'border-2 border-[' + border + 'shadow-lg'">
+        {{ role }}
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        background: {
+            type: String,
+        },
+        border: {
+            type: String,
+        },
+        role: {
+            type: String,
+        }
+    }
+}
+
+</script>
---
+++ app/pages/bento2.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-screen h-screen p-[100px] flex justify-center items-center">
+      <div class="w-full h-full grid grid-cols-7 grid-rows-8 gap-5">
+          <div class="w-full h-full col-span-2 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+          <div class="w-full h-full col-span-3 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+          <div class="w-full h-full col-span-2 row-span-8 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+          <div class="w-full h-full col-span-5 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+      </div>
+  </div>
+</template>
