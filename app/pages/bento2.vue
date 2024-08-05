7976d77db543ba3f66d9c4b71801496c4ffa937c
ZachLTech <zlopezzl2021@gmail.com>
2024-08-05 13:41:42 -0400

    bento 2 for OSS/future usecase

---
+++ app/pages/bento2.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-screen h-screen p-[100px] flex justify-center items-center">
+        <div class="w-full h-full grid grid-cols-7 grid-rows-8 gap-3">
+            <div id="skillCycle" class="w-full h-full col-span-1 row-span-8 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+            <div id="intro" class="w-full h-full col-span-2 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+            <div id="stats" class="w-full h-full col-span-4 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+            <div id="projects" class="w-full h-full col-span-4 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+            <div id="availability" class="w-full h-full col-span-2 row-span-4 flex justify-center items-center font-bold text-5xl bg-[#010101] border-2 rounded-3xl border-[#dbe7f4] shadow-lg"></div>
+        </div>
+    </div>
+  </template>
