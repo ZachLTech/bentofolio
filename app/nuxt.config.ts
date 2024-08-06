87c7904493e27461592d12ab334bfc29d6059a84
ZachLTech <zlopezzl2021@gmail.com>
2024-08-06 02:18:00 -0400

    SEO :D

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -10,6 +10,14 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ['nuxt-swiper', "nuxt-viewport"],
+  modules: ['nuxt-swiper', "nuxt-viewport", "@nuxtjs/seo"],
+
+  site: {
+    url: 'https://bentofolio.zachl.tech',
+    name: 'Zach\'s Bentofolio',
+    description: 'A bento grid style portfolio to show off my skills and experience :D - a pocket portfolio!',
+    defaultLocale: 'en',
+  },
+
   compatibilityDate: '2024-07-29',
 })
